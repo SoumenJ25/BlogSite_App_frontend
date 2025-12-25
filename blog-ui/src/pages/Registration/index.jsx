75f95a59e688295e44206afcbdbfe5c7c23c2ce6
SoumenJ25 <soumen.jana@cognizant.com>
2025-12-25 21:49:08 +0530

    feat: created axios instance, implemented form validations for registration, integrated api with registration page

--- blog-ui/src/pages/Registration/index.jsx
+++ blog-ui/src/pages/Registration/index.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Typography, TextField, Button, Paper } from '@mui/material'
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import api from '../../api/axios.js'
 import './Registration.css'
 
 const Registration = () => {
+    const [formValues, setFormValues] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const [errors, setErrors] = useState({})
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const validateForm = () => {
+        const newErrors = {}
+
+        if (!formValues.username.trim()) {
+            newErrors.username = 'Username is required'
+        }
+
+        if (!formValues.email.trim()) {
+            newErrors.email = 'Email is required'
+        } else if (!/^\S+@\S+\.\S+$/.test(formValues.email)) {
+            newErrors.email = 'Enter a valid email address'
+        }
+
+        if (!formValues.password) {
+            newErrors.password = 'Password is required'
+        } else if (!/^[a-zA-Z0-9]+$/.test(formValues.password)) {
+            newErrors.password = 'Password must be alphanumeric'
+        }
+        else if (formValues.password.length < 7) {
+            newErrors.password = 'Password must be of at least 8 characters '
+        }
+
+        if (!formValues.confirmPassword) {
+            newErrors.confirmPassword = 'Confirm Password is required'
+        } else if (formValues.password !== formValues.confirmPassword) {
+            newErrors.confirmPassword = 'Passwords do not match'
+        }
+
+        setErrors(newErrors)
+        return Object.keys(newErrors).length === 0
+    }
+
+    const handleSubmit = async () => {
+        if (!validateForm()) return
+
+        try {
+            const response = await api.post('/user/register', {
+                username: formValues.username,
+                email: formValues.email,
+                password: formValues.password
+            })
+            console.log('remote logs: Registration success', response?.data)
+            alert('User is registered successfully !!')
+            navigate('/login')
+
+        } catch (error) {
+            console.error('remote logs: Registration Error:', error.response?.data || error.message)
+        }
+    }
+
     return (
         <Box className="register-page">
             <Paper elevation={6} className="register-card">
@@ -19,34 +87,55 @@ const Registration = () => {
                 <Box className="register-form">
                     <TextField
                         label="Username"
+                        name="username"
                         fullWidth
                         margin="normal"
+                        value={formValues.username}
+                        onChange={handleChange}
+                        error={!!errors.username}
+                        helperText={errors.username}
                     />
 
                     <TextField
                         label="Email"
+                        name="email"
                         fullWidth
                         margin="normal"
+                        value={formValues.email}
+                        onChange={handleChange}
+                        error={!!errors.email}
+                        helperText={errors.email}
                     />
 
                     <TextField
                         label="Password"
+                        name="password"
                         type="password"
                         fullWidth
                         margin="normal"
+                        value={formValues.password}
+                        onChange={handleChange}
+                        error={!!errors.password}
+                        helperText={errors.password}
                     />
 
                     <TextField
                         label="Confirm Password"
+                        name="confirmPassword"
                         type="password"
                         fullWidth
                         margin="normal"
+                        value={formValues.confirmPassword}
+                        onChange={handleChange}
+                        error={!!errors.confirmPassword}
+                        helperText={errors.confirmPassword}
                     />
 
                     <Button
                         variant="contained"
                         fullWidth
                         className="create-account-btn"
+                        onClick={handleSubmit}
                     >
                         CREATE ACCOUNT
                     </Button>
@@ -54,7 +143,7 @@ const Registration = () => {
 
                 <Typography variant="body2" className="login-redirect">
                     Already have an account?{' '}
-                    <Link to='/login' className='signin-link'>
+                    <Link to="/login" className="signin-link">
                         Sign in
                     </Link>
                 </Typography>
